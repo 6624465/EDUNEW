694e271d4f6e3d409dc0c842970748f4c798cbe9
Prasad SKD <prasad.siripireddi@gmail.com>
2017-11-28 11:43:46 +0530

    commit

---
+++ EDU.Web/Controllers/FinancialTransactionController.cs
@@ -0,0 +1,25 @@
+﻿using EDU.Web.Models;
+using EZY.EDU.BusinessFactory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EDU.Web.Controllers
+{
+    public class FinancialTransactionController : Controller
+    {
+        EducationEntities dbContext = new EducationEntities();
+
+        [HttpGet]
+        public ActionResult FinancialTransactionList()
+        {
+            ViewData["CountryData"] = new BranchBO().GetList().Where(x => x.IsActive == true).ToList();
+            ViewData["CurrencyList"] = new LookupBO().GetList().Where(x => x.LookupCategory == "Currency").ToList();
+            ViewData["TrainingConfirmation"] = dbContext.TrainingConfirmations.Where(x => x.IsActive == true).ToList();
+
+            return View();
+        }
+    }
+}
