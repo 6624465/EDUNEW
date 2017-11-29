73d8693641cda3e410eb8c3db4f48759605d1a5f
Prasad SKD <prasad.siripireddi@gmail.com>
2017-11-29 12:55:12 +0530

    commit changes

--- EDU.Web/Controllers/FinancialTransactionController.cs
+++ EDU.Web/Controllers/FinancialTransactionController.cs
@@ -4,6 +4,7 @@ using EZY.EDU.BusinessFactory;
 using EZY.EDU.Contract;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -46,10 +47,81 @@ namespace EDU.Web.Controllers
             foreach (EDU.Web.Models.Lookup item in FinancialTransactionLookupList)
             {
                 if (financialTransactionVM.financialTransactionDetailList.Where(x => x.DescriptionId == item.LookupID).Count() == 0)
-                    financialTransactionVM.financialTransactionDetailList.Add(new FinancialTransactionDetail() { DescriptionId = item.LookupID, Description=item.LookupDescription, FinancialTransactionId = financialTransaction.FinancialTransactionId, TrainingConfirmationID = financialTransaction.TrainingConfirmationID });
+                    financialTransactionVM.financialTransactionDetailList.Add(new FinancialTransactionDetail() { DescriptionId = item.LookupID, Description = item.LookupDescription, FinancialTransactionId = financialTransaction.FinancialTransactionId, TrainingConfirmationID = financialTransaction.TrainingConfirmationID });
             }
 
             return View(financialTransactionVM);
+        }
+
+        [HttpGet]
+        public PartialViewResult FinancialTransactionDetail(int? financialTransactionId, string trainingConfirmationID, short? descriptionId, decimal? currencyExRate)
+        {
+            ViewData["CurrencyExRate"] = currencyExRate;
+            if (financialTransactionId == -1)
+            {
+                ViewBag.Title = "New Financial Transaction";
+                return PartialView(new FinancialTransactionDetail { FinancialTransactionId = -1, TrainingConfirmationID = trainingConfirmationID, DescriptionId = descriptionId });
+            }
+            else
+            {
+                ViewBag.Title = "Update Financial Transaction";
+                return PartialView(dbContext.FinancialTransactionDetails.Where(x => x.FinancialTransactionId == financialTransactionId && x.TrainingConfirmationID == trainingConfirmationID && x.DescriptionId == descriptionId));
+            }
+        }
+
+        [HttpPost]
+        public JsonResult SaveFinancialTransaction(FinancialTransactionVM ftinfo)
+        {
+            try
+            {
+                if (ftinfo.financialTransaction.FinancialTransactionId == -1)
+                {
+                    ftinfo.financialTransaction.CreatedBy = USER_ID;
+                    ftinfo.financialTransaction.CreatedOn = UTILITY.SINGAPORETIME;
+                    ftinfo.financialTransaction.IsActive = true;
+                    dbContext.FinancialTransactions.Add(ftinfo.financialTransaction);
+
+
+                }
+                else
+                {
+                    FinancialTransactionVM financialTransactionVM = new FinancialTransactionVM();
+
+                    financialTransactionVM.financialTransaction = dbContext.FinancialTransactions.
+                       Where(x => x.FinancialTransactionId == ftinfo.financialTransaction.FinancialTransactionId).FirstOrDefault();
+
+                    financialTransactionVM.financialTransaction.Country = ftinfo.financialTransaction.Country;
+                    financialTransactionVM.financialTransaction.CurrencyCode = ftinfo.financialTransaction.CurrencyCode;
+                    financialTransactionVM.financialTransaction.CurrencyExRate = ftinfo.financialTransaction.CurrencyExRate;
+                    financialTransactionVM.financialTransaction.GrossProfit = ftinfo.financialTransaction.GrossProfit;
+                    financialTransactionVM.financialTransaction.ProfitAndLossPercent = ftinfo.financialTransaction.ProfitAndLossPercent;
+                    financialTransactionVM.financialTransaction.IsActive = true;
+
+                    financialTransactionVM.financialTransaction.ModifiedBy = USER_ID;
+                    financialTransactionVM.financialTransaction.ModifiedOn = UTILITY.SINGAPORETIME;
+
+                    dbContext.Entry(financialTransactionVM.financialTransaction).State = EntityState.Modified;
+                }
+
+                dbContext.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            string message = "Saved Successfully.";
+            return Json(message, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpGet]
+        public JsonResult GetCurrency(Int16 Id)
+        {
+            var country = new BranchBO().GetList().Where(x => x.IsActive == true && x.BranchID == Id).FirstOrDefault();
+            var currencyCode = dbContext.Lookups.Where(x => x.LookupCategory == "Currency" && x.MappingCode == country.BranchCode).FirstOrDefault();
+
+            return Json(currencyCode == null ? "" : currencyCode.LookupCode, JsonRequestBehavior.AllowGet);
+
         }
     }
 }
