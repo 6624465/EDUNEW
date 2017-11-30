7562c9938d60f176a05fb7573db0d74e306563e8
Prasad SKD <prasad.siripireddi@gmail.com>
2017-11-30 17:50:26 +0530

    added Operational transaction controller

---
+++ EDU.Web/Controllers/OperationalTransactionController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EDU.Web.Controllers
+{
+    public class OperationalTransactionController : Controller
+    {
+        // GET: OperationalTransaction
+        public ActionResult OperationalTransactionList(short? month)
+        {
+            return View();
+        }
+
+        public PartialViewResult OperationalTransaction()
+        {
+            return PartialView();
+        }
+    }
+}
