011df80f276fb384e18c2904bd849cea3e2f0b29
Naresh K <Naresh@sulabs.tech>
2017-12-08 18:03:18 +0530

    operation reports changes

--- EDU.Web/Controllers/OperationalTransactionController.cs
+++ EDU.Web/Controllers/OperationalTransactionController.cs
@@ -174,7 +174,7 @@ namespace EDU.Web.Controllers
 
 
                 List<OperationalTransactionReportVM> list = dbContext.OperationalTransactions
-                    .Where(x => x.IsActive == true && x.Country == country)
+                    .Where(x => x.IsActive == true && x.Country == country &&x.Year==year)
                     .Select(y => new OperationalTransactionReportVM
                     {
                         CategoryId = y.CategoryId,
