3965c5f1d63387f94a5d4f0a9d0a15fc93e07ca6
Prasad SKD <prasad.siripireddi@gmail.com>
2017-12-12 14:18:37 +0530

    commit changes

---
+++ EDU.Web/ViewModels/ReportsModel/TotalRevenueVM.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EDU.Web.ViewModels.ReportsModel
+{
+    public class TotalRevenueVM
+    {
+        public int Year { get; set; }
+        public Int16 Country { get; set; }
+        public string CountryName { get; set; }
+        public decimal? TotalRevenue { get; set; }
+        public decimal? AchievedRevenue { get; set; }
+        public decimal? YetToAchieveRevenue { get; set; }
+    }
+}
